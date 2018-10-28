4050c2d04e0df8471edcbda1ba89bc77ce4a16e8
Daniel Barclay <barclaysd@me.com>
2018-10-28 22:16:11 +0000

    Burger builder controls added to increment/decrement fillings with logic

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,8 +5,16 @@ import classes from './BuildControl.css';
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less}>Decrease Quantity</button>
-        <button className={classes.More}>Increase Quantity</button>
+        <button
+            className={classes.Less}
+            onClick={props.removed}
+            disabled={props.disabled}>
+            Decrease Quantity
+        </button>
+        <button className={classes.More}
+                onClick={props.added}>
+            Increase Quantity
+        </button>
     </div>
 );
 

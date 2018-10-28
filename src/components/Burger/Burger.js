6ff99ce8e284e0cea1a61e3759179149bdd9c6dd
Daniel Barclay <daniel@urbanintel.io>
2018-10-28 19:28:34 +0000

    Added burger builder controls function and css

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,21 @@ import BurgerIngredients from "./BurgerIngredients/BurgerIngredents";
 import classes from './Burger.css';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingKey => {
             return [...Array(props.ingredients[ingKey])].map((_, i) => {
                 return <BurgerIngredients key={ingKey + i} type={ingKey}/>
                 });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+
+    console.log(transformedIngredients);
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
 
     return (
         <div className={classes.Burger}>

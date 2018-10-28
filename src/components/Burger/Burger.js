fd64a4b8921e0e3f0772f73ecf1353724b925127
Daniel Barclay <daniel@urbanintel.io>
2018-10-28 18:57:23 +0000

    Dynamic method to render Burger to display based on state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,2 +1,22 @@
 import React from 'react';
+import BurgerIngredients from "./BurgerIngredients/BurgerIngredents";
+import classes from './Burger.css';
 
+const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingKey => {
+            return [...Array(props.ingredients[ingKey])].map((_, i) => {
+                return <BurgerIngredients key={ingKey + i} type={ingKey}/>
+                });
+        });
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredients type='bread-top'/>
+            {transformedIngredients}
+            <BurgerIngredients type='bread-bottom'/>
+        </div>
+    );
+};
+
+export default burger;

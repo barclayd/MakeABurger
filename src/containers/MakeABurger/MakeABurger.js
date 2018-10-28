01e70b5bb7ce5ee09d42870e75d71ac3d92cac65
Daniel Barclay <barclaysd@me.com>
2018-10-28 11:47:13 +0000

    Added components to build the burger and display dynamic CSS

---
+++ src/containers/MakeABurger/MakeABurger.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/Aux';
+
+class MakeABurger extends Component {
+
+    render () {
+
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Aux>
+        )
+    }
+
+}
+export default MakeABurger;

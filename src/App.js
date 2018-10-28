ae9ec82c8bac42cfc0e683a6dcbbc0eaead7bf87
Daniel Barclay <barclaysd@me.com>
2018-10-28 18:57:23 +0000

    Dynamic method to render Burger to display based on state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
-import MakeABurger from "./containers/MakeABurger/MakeABurger";
+import BurgerBuilder from "./containers/MakeABurger/BurgerBuilder";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-            <MakeABurger />
+            <BurgerBuilder />
         </Layout>
       </div>
     );
--- src/components/Burger/BurgerIngredients/BurgerIngredents.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredents.js
@@ -10,13 +10,13 @@ class BurgerIngredients extends Component {
 
     switch(this.props.type) {
         case ('bread-bottom'):
-            ingredients = <div className={classes.BreadBottom}></div>;
+            ingredients = <div className={classes.BreadBottom}></div>
             break;
         case ('bread-top'):
             ingredients = (
                 <div className={classes.BreadTop}>
-                    <div className={classes.Seeds1}></div>;
-                    <div className={classes.Seeds2}></div>;
+                    <div className={classes.Seeds1}></div>
+                    <div className={classes.Seeds2}></div>
                 </div>
             );
             break;
@@ -24,13 +24,13 @@ class BurgerIngredients extends Component {
             ingredients = <div className={classes.Meat}></div>;
             break;
         case ('salad'):
-            ingredients = <div className={classes.Salad}></div>;
+            ingredients = <div className={classes.Salad}></div>
             break;
         case ('cheese'):
-            ingredients = <div className={classes.Cheese}></div>;
+            ingredients = <div className={classes.Cheese}></div>
             break;
         case ('bacon'):
-            ingredients = <div className={classes.Bacon}></div>;
+            ingredients = <div className={classes.Bacon}></div>
             break;
         default:
             ingredients = null;

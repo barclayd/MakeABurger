081073c790525410902ad89bdd29acbfe1143831
Daniel Barclay <daniel@urbanintel.io>
2018-10-30 22:52:32 +0000

    Added dynamic hamburger menu for mobile view with onClickListener

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import MenuToggle from '../../Navigation/SideDrawer/MenuToggle/MenuToggle';
 import classes from './Toolbar.css';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <div className={classes.menuToggle}>
+            <MenuToggle
+                isSideDrawOpen={props.openSideDrawer}
+                clicked={props.toggleSideBar} />
+        </div>
         <div className={classes.Logo}>
             <Logo height='80%'/>
         </div>

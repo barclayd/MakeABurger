3554a298a9f00ebddc3b87ec9d9a7e511e95f2e6
Daniel Barclay <barclaysd@me.com>
2018-11-05 20:33:41 +0000

    Fixed active page styling CSS bug

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -6,8 +6,8 @@ const navigationItem = (props) => (
         <li className={classes.NavigationItem}>
             <NavLink
                 to={props.link}
-                exact
-                activeClassName={classes.NavigationItem}>
+                exact={props.exact}
+                activeClassName={classes.active}>
                 {props.children}
                 </NavLink>
         </li>
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,7 +6,7 @@ const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
             <NavigationItem
                 link='/'
-                exact={props.exact}>
+                exact>
                     Burger Builder
             </NavigationItem>
             <NavigationItem

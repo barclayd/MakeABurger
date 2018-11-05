18f8cb10f2efa3b92c958c06870c0976d87e5953
Daniel Barclay <barclaysd@me.com>
2018-11-05 18:15:56 +0000

    Order submission to Firebase dB and passed data between components

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
+import axios from '../../../axios-orders';
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css'
 
 class ContactData extends Component {
@@ -11,21 +13,63 @@ class ContactData extends Component {
             houseName: '',
             street: '',
             postcode: ''
-        }
+        },
+        loading: false
+    };
+
+    orderHandler = (event) => {
+        // prevents <form> default to reload page to send a request
+        event.preventDefault();
+        this.setState({
+                    loading: true
+                });
+                // alert('You have chosen to continue!');
+                const order = {
+                    ingredients: this.props.ingredients,
+                    price: this.props.price,
+                    customer: {
+                        name: 'Daniel Barclay',
+                        address: {
+                            street: 'Salisbury Road',
+                            postcode: 'CF24 4AD',
+                            country: 'United Kingdom'
+                        },
+                        email: 'test@test.com'
+                    },
+                    deliveryMethod: 'deliveroo'
+        };
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({
+                    loading: false
+                });
+                this.props.history.push('/');
+            })
+            .catch(error => {
+                this.setState({
+                    loading: false
+                });
+            })
     };
 
     render() {
-
+        let form =
+            (
+            <form>
+                <input className={classes.Input} type='text' name='name' placeholder='Your name' />
+                <input className={classes.Input} type='email' name='email' placeholder='Your email' />
+                <input className={classes.Input} type='text' name='street' placeholder='Street Address' />
+                <input className={classes.Input} type='text' name='postal' placeholder='Post Code' />
+                <Button btnType='Success' clicked={this.orderHandler}>PLACE ORDER</Button>
+            </form>
+            );
+        if(this.state.loading) {
+            form = <Spinner />;
+        }
         return (
             <div className={classes.ContactData}>
-                <h4>Contact Data</h4>
-                <form>
-                    <input className={classes.Input} type='text' name='name' placeholder='Your name' />
-                    <input className={classes.Input} type='email' name='email' placeholder='Your email' />
-                    <input className={classes.Input} type='text' name='street' placeholder='Street Address' />
-                    <input className={classes.Input} type='text' name='postal' placeholder='Post Code' />
-                    <Button btnType='Success'>PLACE ORDER</Button>
-                </form>
+                <h4>Please enter delivery information</h4>
+                {form}
             </div>
 
         )

199720ac34a92c9f497c9d9ca8610bd047fa5e4f
Daniel Barclay <barclaysd@me.com>
2018-11-13 23:17:39 +0000

    Added custom logging middleware for Redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
@@ -10,7 +10,18 @@ import * as serviceWorker from './serviceWorker';
 
 const rootReducer = orderReducer;
 
-const store = createStore(rootReducer);
+const logger = (store) => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState());
+            return result;
+        }
+    };
+};
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 const app = (
     <Provider store={store}>

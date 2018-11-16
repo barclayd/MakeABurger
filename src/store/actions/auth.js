87504aee521af588cd8f707fa37cc93a8f65abfb
Daniel Barclay <barclaysd@me.com>
2018-11-16 09:56:29 +0000

    Added a spinner to display whilst waiting for response from server. Detailed login error messages now displayed

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -40,8 +40,8 @@ export const auth = (email, password, isSignUp) => {
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
             })
             .catch(err => {
-                console.log(err);
-                dispatch(authFail(err));
+                console.log(err.response.data.error.message);
+                dispatch(authFail(err.response.data.error));
             })
     };
 };

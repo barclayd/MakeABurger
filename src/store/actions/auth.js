ef00ea5bd5a83c6101be3397cbe1799931fb7cf4
Daniel Barclay <barclaysd@me.com>
2018-11-22 01:20:02 +0000

    Fixed bug that led to sign-in error on page refresh and moved BurgerBuilder side effects to a Saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,4 @@
-import * as actionTypes from './actionTypes';
-import axios from 'axios';
+import * as actionTypes from "./actionTypes";
 
 export const authStart = () => {
     return {
@@ -15,7 +14,7 @@ export const authSuccess = (token, userId) => {
     };
 };
 
-export const authFail = (error) => {
+export const authFail = error => {
     return {
         type: actionTypes.AUTH_FAIL,
         error: error
@@ -23,77 +22,43 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
-    // localStorage.removeItem('token');
-    // localStorage.removeItem('expirationDate');
-    // localStorage.removeItem('userId');
     return {
-        type: actionTypes.AUTH_INITIATE_LOGOUT,
+        type: actionTypes.AUTH_INITIATE_LOGOUT
     };
 };
 
 export const logoutSucceed = () => {
     return {
-        type: actionTypes.AUTH_LOGOUT,
-    }
-};
-
-export const checkAuthTimeout = (expirationTime) => {
-    return {
-        type: actionTypes.AUTH_CHECK_TIMEOUT,
-        expirationTime: expirationTime
-    }
-};
-
-export const auth = (email, password, isSignUp) => {
-    return dispatch => {
-        dispatch(authStart());
-        const authData = {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCVlTlc5XiDZeLCHSO-TEPOr78biVPESTY';
-        if(!isSignUp) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCVlTlc5XiDZeLCHSO-TEPOr78biVPESTY'
-        }
-        axios.post(url, authData)
-            .then(response => {
-                const expirationDate = new Date(new Date().getTime() + (response.data.expiresIn * 1000));
-                localStorage.setItem('token',response.data.idToken);
-                localStorage.setItem('expirationDate', expirationDate);
-                localStorage.setItem('userId', response.data.localId);
-                dispatch(authSuccess(response.data.idToken, response.data.localId));
-                dispatch(checkAuthTimeout(response.data.expiresIn))
-            })
-            .catch(err => {
-                dispatch(authFail(err.response.data.error));
-            })
+        type: actionTypes.AUTH_LOGOUT
     };
 };
 
-export const setAuthRedirectPath = (path) => {
+export const checkAuthTimeout = expirationTime => {
+    return {
+        type: actionTypes.AUTH_CHECK_TIMEOUT,
+        expirationTime: expirationTime
+    };
+};
+
+export const auth = (email, password, isSignup) => {
+    console.log(isSignup);
+    return {
+        type: actionTypes.AUTH_USER,
+        email: email,
+        password: password,
+        isSignup: isSignup
+    };
+};
+
+export const setAuthRedirectPath = path => {
     return {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
-    }
+    };
 };
 
 export const authCheckState = () => {
-    return dispatch => {
-        const token = localStorage.getItem('token');
-        if(!token) {
-            dispatch(logout());
-        } else {
-            const expirationTime = new Date(localStorage.getItem('expirationDate'));
-            if(expirationTime > new Date ()) {
-                const userId = localStorage.getItem('userId');
-                dispatch(authSuccess(token, userId));
-                dispatch(checkAuthTimeout((expirationTime.getTime() - new Date().getSeconds())/1000));
-            } else {
-                dispatch(logout());
-            }
-        }
-
-    }
-
+    return {
+        type: actionTypes.AUTH_CHECK_STATE
+    };
 };
